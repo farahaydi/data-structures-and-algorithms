69b1dbb281db49194cd7218a6209b8afd5e2e08e
farahisJoy <farahelaydi9@gmail.com>
2023-09-04 08:51:38 +0000

    updated

--- StackQueue/app/src/main/java/stackqueue/PseudoQueue.java
+++ StackQueue/app/src/main/java/stackqueue/PseudoQueue.java
@@ -12,18 +12,24 @@ public class PseudoQueue {
     //enqueue
     //Arguments: value
     //Inserts a value into the PseudoQueue, using a first-in, first-out approach.
-    public void enqueue(int value)
-    {
-        Node newValue = new Node(value);
-        FistStack.push(newValue.item);
+    public void enqueue(int value) {
+        Node newNode = new Node(value); //20-15
+        while (!FistStack.isEmpty()) {
+            SecondStack.push(FistStack.pop()); //Second ->20
+        }
+        FistStack.push(newNode.item); //First -> 15-20
+        while (!SecondStack.isEmpty()) {
+            FistStack.push(SecondStack.pop());
+        }
         length++;
     }
+
     //dequeue
     //Arguments: none
     //Extracts a value from the PseudoQueue, using a first-in, first-out approach.
     public int dequeue()
     {
-        if (FistStack.isEmpty() && SecondStack.isEmpty())
+        if (FistStack.isEmpty() || SecondStack.isEmpty())
         {
             throw new IllegalArgumentException("The PseudoQueue is empty");
         }
@@ -34,6 +40,32 @@ public class PseudoQueue {
             return SecondStack.pop();
 
     }
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Queue: ");
+        LinckedStack tempStack = new LinckedStack();
+
+        while (!SecondStack.isEmpty()) {
+            tempStack.push(SecondStack.pop());
+        }
+
+        while (!tempStack.isEmpty()) {
+            int item = tempStack.pop();
+            result.append(item).append(" ");
+            SecondStack.push(item);
+        }
+
+        while (!FistStack.isEmpty()) {
+            tempStack.push(FistStack.pop());
+        }
+
+        while (!tempStack.isEmpty()) {
+            int item = tempStack.pop();
+            result.append(item).append(" ");
+            SecondStack.push(item);
+        }
+        return result.toString();
+}
 
 
 
