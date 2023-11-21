c1bec7db3117c2f7211d01becb9fce1933ca36a2
farahisJoy <farahelaydi9@gmail.com>
2023-11-21 17:16:05 +0000

    cc37

--- Graph/app/src/main/java/graph/App.java
+++ Graph/app/src/main/java/graph/App.java
@@ -1,36 +1,73 @@
 
 package graph;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class App {
 
     public static void main(String[] args) {
+        Graph graph1 = new Graph();
+
+        Object vertexA = graph1.addVertex("A");
+        Object vertexB = graph1.addVertex("B");
+        Object vertexC = graph1.addVertex("C");
+        Object vertexD = graph1.addVertex("D");
+        Object vertexE = graph1.addVertex("E");
+
+        graph1.addEdge(vertexA, vertexB, 2);
+        graph1.addEdge(vertexA, vertexC, 1);
+        graph1.addEdge(vertexB, vertexC, 3);
+        graph1.addEdge(vertexB, vertexD, 4);
+        graph1.addEdge(vertexC, vertexE, 5);
+
+        System.out.println("Vertices: " + graph1.getVertices());
+
+        System.out.println("Neighbors of A: " + graph1.getNeighbors(vertexA));
+
+        System.out.println("Size of the graph: " + graph1.size());
+
+        System.out.println("Breadth-first traversal:");
+        graph1.breadthFirst(vertexA);
+
+
+
         Graph graph = new Graph();
 
-        // Adding vertices
-        Object vertexA = graph.addVertex("A");
-        Object vertexB = graph.addVertex("B");
-        Object vertexC = graph.addVertex("C");
-        Object vertexD = graph.addVertex("D");
-        Object vertexE = graph.addVertex("E");
+        graph.addVertex("Metroville");
+        graph.addVertex("Pandora");
+        graph.addEdge("Metroville", "Pandora", 82);
 
-        // Adding edges
-        graph.addEdge(vertexA, vertexB, 2);
-        graph.addEdge(vertexA, vertexC, 1);
-        graph.addEdge(vertexB, vertexC, 3);
-        graph.addEdge(vertexB, vertexD, 4);
-        graph.addEdge(vertexC, vertexE, 5);
+        graph.addVertex("Arendelle");
+        graph.addVertex("New Monstropolis");
+        graph.addVertex("Naboo");
+        graph.addEdge("Arendelle", "New Monstropolis", 45);
+        graph.addEdge("Arendelle", "Naboo", 73);
+        graph.addEdge("New Monstropolis", "Naboo", 90);
 
-        // Getting vertices
-        System.out.println("Vertices: " + graph.getVertices());
+        graph.addVertex("Narnia");
+        graph.addEdge("Narnia", "Arendelle", 37);
+        graph.addEdge("Narnia", "Naboo", 130);
 
-        // Getting neighbors of a vertex
-        System.out.println("Neighbors of A: " + graph.getNeighbors(vertexA));
+        String[] trip1 = {"Metroville", "Pandora"};
+        Integer cost1 = graph.businessTrip(trip1);
+        System.out.println("Input: " + Arrays.toString(trip1) + "\tOutput: $" + cost1);
 
-        // Getting the size of the graph
-        System.out.println("Size of the graph: " + graph.size());
+        String[] trip2 = {"Arendelle", "New Monstropolis", "Naboo"};
+        Integer cost2 = graph.businessTrip(trip2);
+        System.out.println("Input: " + Arrays.toString(trip2) + "\tOutput: $" + cost2);
 
-        // Breadth-first traversal starting from vertexA
-        System.out.println("Breadth-first traversal:");
-        graph.breadthFirst(vertexA);
+        String[] trip3 = {"Naboo", "Pandora"};
+        Integer cost3 = graph.businessTrip(trip3);
+        System.out.println("Input: " + Arrays.toString(trip3) + "\tOutput: $" + cost3);
+
+        String[] trip4 = {"Narnia", "Arendelle", "Naboo"};
+        Integer cost4 = graph.businessTrip(trip4);
+        System.out.println("Input: " + Arrays.toString(trip4) + "\tOutput: $" + cost4);
     }
-}
+
+
+    }
+
